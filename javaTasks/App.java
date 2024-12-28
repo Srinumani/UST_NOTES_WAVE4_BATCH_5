d12960d8b732e4bf97e99a134c3cad23e4cc8e12
srinumanikanta katha <srinumanikanta0011@gmail.com>
2024-12-28 12:17:29 +0530

    firsttaskcreated

---
+++ javaTasks/App.java
@@ -0,0 +1,24 @@
+
+public class App{
+    public static void main(String args[]){
+
+        // execute this code in cmd , if you dont know follow bellow line
+
+        // open editior create a file like Mani
+
+        // create a one class filename and class name must be same 
+
+        // avigate to the folder where you saved the Mani.java file. Use the cd command cd c:/foldername
+
+        // after the completion of code go to cmd ---> javac Mani.java
+
+        //cmd --->java Mani Manikanta
+
+        //output 
+            //Hello Mani
+            //Hello Manikanta
+
+        System.out.println("Hello Mani");
+        System.out.println("Hello "+args[0]);
+    }
+}
